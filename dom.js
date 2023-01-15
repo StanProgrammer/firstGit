647b263b9927aafe65ea87bf3b49ace4aafedbb8
AtibProgrammer <atibkhan392@gmail.com>
2023-01-15 11:47:28 +0530

    changes made to item

--- dom.js
+++ dom.js
@@ -13,11 +13,24 @@ console.log(title)
 // title.textContent='Add Now'
 
 var header=document.getElementById('main-header');
+var headerTitle=document.getElementById('header-title')
 header.style.borderBottom='5px solid black';
 console.log(header.textContent)
 
 const items=document.querySelector('.title');
 items.style.fontWeight = "bold"
 items.style.color = "green"
+
+headerTitle.innerHTML='<h3>Hello<h3>'
+
+const item1=document.getElementsByClassName('list-group-item');
+// for(let i of item1){
+//     console.log(i)
+// }
+item1[2].style.backgroundColor='green'
+// item1[].style.fontWeight='bold'
+for(let i of item1){
+    i.style.fontWeight='bold';
+}
 
 
