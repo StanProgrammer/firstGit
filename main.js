9111d6263c17eede06e5769bff7c5c0a00329276
AtibProgrammer <atibkhan392@gmail.com>
2023-01-16 18:33:04 +0530

    Objective Acheived

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ form.addEventListener('submit',additem)
 function additem(e){
     e.preventDefault();
     const newItem=document.getElementById('item').value
+    const newItem1=document.getElementById('item1').value
     
     // Create Element
     const li= document.createElement('li')
-    li.className='list-group-item'
-    li.appendChild(document.createTextNode(newItem))
+    li.className='list-group-item '
+    // li.appendChild(document.createTextNode(`${newItem} ${newItem1}`))
+    li.appendChild(document.createTextNode(`${newItem} `))
+    li.appendChild(document.createTextNode(`${newItem1}`))
 
     //Create Button
     const deleteBtn=document.createElement('button')
@@ -19,7 +22,12 @@ function additem(e){
 
     li.appendChild(deleteBtn)
     itemList.appendChild(li)
-    
+
+    const editBtn=document.createElement('button')
+    editBtn.className='btn btn-primary btn-sm float-right mr-1'
+    li.appendChild(editBtn)
+    editBtn.appendChild(document.createTextNode('edit'))
+    itemList.appendChild(li)
     
 }
 
@@ -32,5 +40,21 @@ function removeItem(e){
         }
     }
 }
-
+const filter=document.getElementById('filter');
+filter.addEventListener('keyup',filterItems)
+function filterItems(e){
+    var text=e.target.value.toLowerCase()
+    var items=itemList.getElementsByTagName('li')
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent;
+        var itemName1=item.firstChild.nextSibling.textContent;
+        // console.log(itemName1)
+        if(itemName.toLowerCase().indexOf(text)!=-1 || itemName1.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
+}
 
